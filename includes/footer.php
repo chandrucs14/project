aa38464bcb5d0d8463d6803b4faee76e5b9033d1
chandrucs14 <chandruchandru39367@gmail.com>
2026-03-16 13:11:33 +0530

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
                 <div class="row">
                     <div class="col-12">
                         ©
-                        <script>document.write(new Date().getFullYear())</script> Fonik<span class="d-none d-sm-inline-block"> -
-                            Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand.</span>
+                        <script>document.write(new Date().getFullYear())</script> VKM Construction <span class="d-none d-sm-inline-block"> -
+                            Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="ecommer.in">Ecommer</a>.</span>
                     </div>
                 </div>
             </div>
--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8" />
-    <title>Dashboard | Fonik - Responsive Bootstrap 5 Admin Dashboard</title>
+    <title>VKM Construction</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
